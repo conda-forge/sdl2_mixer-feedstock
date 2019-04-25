e125606d8d82a5ba037c5620fe2ccbc8f7fd11af
matham <moiein2000@gmail.com>
2019-04-24 22:35:56 -0400

    Fix vs2008.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,6 +6,22 @@ if %ARCH%==32 (
 	set PLATFORM=x64
 )
 
+:: See https://github.com/conda-forge/staged-recipes/pull/194#issuecomment-203577297
+:: Nasty workaround. Need to move a more current msbuild into PATH.  The one on
+:: AppVeyor barfs on the solution. This one comes from the Win7 SDK (.net 4.0),
+:: and is known to work.
+if %VS_MAJOR% == 9 (
+    COPY C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe .\
+    if errorlevel 1 exit 1
+    set "PATH=%CD%;%PATH%"
+)
+
+if "%VS_YEAR%" == "2008" (
+  copy %LIBRARY_INC%\stdint.h %SRC_DIR%\builds\msvc\
+  copy %LIBRARY_INC%\inttypes.h %SRC_DIR%\builds\msvc\
+  if errorlevel 1 exit 1
+)
+
 cd VisualC
 if %VS_MAJOR% GTR 10 (
 	devenv SDL_mixer.sln /upgrade
@@ -36,3 +52,8 @@ echo "prefix does not exists!!!"
 
 msbuild /nologo SDL_mixer.sln "/p:Configuration=Release;Platform=%PLATFORM%;useenv=true"
 if errorlevel 1 exit 1
+
+
+move %PLATFORM%\Release\SDL2_mixer.lib %LIBRARY_LIB%
+move %PLATFORM%\Release\SDL2_mixer.dll %LIBRARY_BIN%
+move ..\SDL_mixer.h %LIBRARY_INC%\\SDL2
