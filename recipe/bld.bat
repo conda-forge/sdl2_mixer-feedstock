b7dbc6961e00eb25e6de25ce33d23ec65d48ba14
H. Vetinari <h.vetinari@gmx.com>
2022-07-17 21:06:09 +0200

    add fluidsynth, exclude opusfile on win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,13 +3,14 @@
 mkdir build
 cd build
 
-:: no modplug/libxmp in conda-forge yet
+:: no libxmp/modplug in conda-forge yet; no opusfile for win
 cmake -G Ninja ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DBUILD_SHARED_LIBS=ON ^
     -DSDL2MIXER_VENDORED=OFF ^
     -DSDL2MIXER_MOD_MODPLUG=OFF ^
     -DSDL2MIXER_MOD_XMP=OFF ^
+    -DSDL2MIXER_OPUS=OFF ^
     ..
 if %ERRORLEVEL% neq 0 exit 1
 
