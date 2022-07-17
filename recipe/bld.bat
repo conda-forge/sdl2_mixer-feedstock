6526a086ee6061e318b92d0b8a47cd12588c63f4
H. Vetinari <h.vetinari@gmx.com>
2022-07-17 19:39:35 +0200

    use ninja

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,10 +3,11 @@
 mkdir build
 cd build
 
-cmake -DCMAKE_BUILD_TYPE=Release ^
-      -DBUILD_SHARED_LIBS=ON ^
-      -DSDL2MIXER_VENDORED=OFF ^
-      ..
+cmake -G Ninja ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DBUILD_SHARED_LIBS=ON ^
+    -DSDL2MIXER_VENDORED=OFF ^
+    ..
 if %ERRORLEVEL% neq 0 exit 1
 
 cmake --build .
--- recipe/build.sh
+++ recipe/build.sh
@@ -4,10 +4,11 @@ set -ex
 mkdir build
 cd build
 
-cmake -DCMAKE_BUILD_TYPE=Release \
-      -DBUILD_SHARED_LIBS=ON \
-      -DSDL2MIXER_VENDORED=OFF \
-      ..
+cmake -G Ninja \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_SHARED_LIBS=ON \
+    -DSDL2MIXER_VENDORED=OFF \
+    ..
 
 cmake --build .
 cmake --install --prefix $PREFIX
