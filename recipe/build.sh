d292700d1d05a6adec929704efec5f62a5e56790
H. Vetinari <h.vetinari@gmx.com>
2022-07-17 21:53:56 +0200

    fixes

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,7 @@ cd build
 # no modplug/libxmp in conda-forge yet
 cmake -G Ninja \
     -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_LIBDIR=lib \
     -DBUILD_SHARED_LIBS=ON \
     -DSDL2MIXER_VENDORED=OFF \
     -DSDL2MIXER_MOD_MODPLUG=OFF \
