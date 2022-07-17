2d5acf343d20a48c98884ac29cf84e966185ffdf
H. Vetinari <h.vetinari@gmx.com>
2022-07-17 21:31:30 +0200

    exclude fluidsynth on win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,11 +3,12 @@
 mkdir build
 cd build
 
-:: no libxmp/modplug in conda-forge yet; no opusfile for win
+:: no libxmp/modplug in conda-forge yet; no fluidsynth/opusfile for win
 cmake -G Ninja ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DBUILD_SHARED_LIBS=ON ^
     -DSDL2MIXER_VENDORED=OFF ^
+    -DSDL2MIXER_MIDI_FLUIDSYNTH=OFF ^
     -DSDL2MIXER_MOD_MODPLUG=OFF ^
     -DSDL2MIXER_MOD_XMP=OFF ^
     -DSDL2MIXER_OPUS=OFF ^
