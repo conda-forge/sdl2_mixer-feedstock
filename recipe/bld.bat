f6f9bf687a139fb9b276b0c02ac4738778afeaff
H. Vetinari <h.vetinari@gmx.com>
2022-07-17 20:31:39 +0200

    disable some modules

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,6 +7,8 @@ cmake -G Ninja ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DBUILD_SHARED_LIBS=ON ^
     -DSDL2MIXER_VENDORED=OFF ^
+    -DSDL2MIXER_MOD_MODPLUG=OFF ^
+    -DSDL2MIXER_MOD_XMP=OFF ^
     ..
 if %ERRORLEVEL% neq 0 exit 1
 
--- recipe/build.sh
+++ recipe/build.sh
@@ -8,6 +8,9 @@ cmake -G Ninja \
     -DCMAKE_BUILD_TYPE=Release \
     -DBUILD_SHARED_LIBS=ON \
     -DSDL2MIXER_VENDORED=OFF \
+    # no modplug/libxmp in conda-forge yet
+    -DSDL2MIXER_MOD_MODPLUG=OFF \
+    -DSDL2MIXER_MOD_XMP=OFF \
     ..
 
 cmake --build .
